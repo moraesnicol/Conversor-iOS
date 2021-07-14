dcd804564cbf69fd8ab099a5a187b4bd53fc3f47
Gabriel <moraesnicol@gmail.com>
2021-07-14 13:19:17 -0300

    ui ok

--- Conversor-iOS/ViewController.swift
+++ Conversor-iOS/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var textFieldValue: UITextField!
+    
+    @IBOutlet weak var buttonUnit1: UIButton!
+    
+    
+    @IBOutlet weak var buttonUnit2: UIButton!
+    
+    
+    
+    @IBOutlet weak var labelResult: UILabel!
+    
+    
+    @IBOutlet weak var labelUnit: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func showNext(_ sender: UIButton) {
+    }
+    
+    @IBAction func convert(_ sender: UIButton) {
+    }
+    
 }
 
